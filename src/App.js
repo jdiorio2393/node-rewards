89251ec73b111b3a6a2a2afc90acd2c565a05703
Jake Diorio <jake.diorio@aexp.com>
2022-02-08 21:27:54 -0500

    feat(scaffolding): create initial app skepeton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Nav from './components/navigation/Nav.jsx'
+import './index.css'
+
+const App = () => {
+   return (
+      <>
+         <Nav />
+         <div>
+            Hello World
+         </div>
+      </>
+   )
+}
+
+export default App
